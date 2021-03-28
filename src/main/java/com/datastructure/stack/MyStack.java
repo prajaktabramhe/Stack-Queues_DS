ca1e5adc47e376b709cd26b4849cf2eb45401e37
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-28 20:28:30 +0530

    [Prajakta]: Added pop and peak method

--- src/main/java/com/datastructure/stack/MyStack.java
+++ src/main/java/com/datastructure/stack/MyStack.java
@@ -23,4 +23,8 @@ public class MyStack
     {
         return myLinkedList.head;
     }
+    public INode pop()
+    {
+        return myLinkedList.pop();
+    }
 }
--- src/test/java/com/datastructure/stack/MyStackTest.java
+++ src/test/java/com/datastructure/stack/MyStackTest.java
@@ -21,6 +21,19 @@ public class MyStackTest
         Assertions.assertEquals(myThirdNode, peak);
 
     }
+    @Test
+    public void given3NumbersInStack_WhenPoped_ShouldMatchWithLastAddedNode() {
+        MyStack myStack = new MyStack();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        INode pop = myStack.pop();
+        myStack.printStack();
+        Assertions.assertEquals(myThirdNode, pop);
+    }
 
 }
 
