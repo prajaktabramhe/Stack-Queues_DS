160a416d95e4442b2387b7c1e2e125ae407e1f1a
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-28 20:23:23 +0530

    [Prajakta]: Created stackk of 56-30-70

---
+++ src/main/java/com/datastructure/stack/INode.java
@@ -0,0 +1,10 @@
+package com.datastructure.stack;
+
+public interface INode<K>
+{
+    K getKey();
+    void setKey (K key);
+
+    INode getNext();
+    void setNext(INode next);
+}
---
+++ src/main/java/com/datastructure/stack/MyLinkedList.java
@@ -0,0 +1,144 @@
+package com.datastructure.stack;
+
+public class MyLinkedList
+{
+    public   INode head;
+    public   INode tail;
+
+    public MyLinkedList()
+    {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void add(INode newNode)
+    {
+
+        if (this.tail == null)
+        {
+            this.tail = newNode;
+        }
+        if (this.head == null)
+        {
+            this.head = newNode;
+        }
+        else
+        {
+            INode tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    public void append(INode myNode)
+    {
+        if (this.head == null)
+        {
+            this.head = myNode;
+        }
+        if (this.tail == null)
+        {
+            this.tail = myNode;
+        } else
+        {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
+        }
+    }
+    public void printMyNodes()
+    {
+        StringBuffer myNodes = new StringBuffer("My Nodes: ");
+        System.out.println("print statement");
+        INode tempNode = head;
+        while (tempNode.getNext() != null)
+        {
+            myNodes.append(tempNode.getKey());
+            if (!tempNode.equals(tail)) myNodes.append("->");
+            tempNode = tempNode.getNext();
+        }
+
+        myNodes.append((tempNode.getKey()));
+        System.out.println(myNodes);
+    }
+
+    public void insert(INode myNode, INode newNode)
+    {
+        INode tempNode = myNode.getNext();
+        myNode.setNext(newNode);
+        newNode.setNext(tempNode);
+    }
+
+    public INode pop()
+    {
+        INode tempNode = this.head;
+        this.head = head.getNext();
+        return tempNode;
+    }
+
+    public INode popLast()
+    {
+        INode tempNode = head;
+        while (!tempNode.getNext().equals((tail))){
+            tempNode = tempNode.getNext();
+        }
+        this.tail = tempNode;
+        tempNode = tempNode.getNext();
+        return tempNode;
+    }
+
+    public INode search(INode searchKey)
+    {
+        INode tempNode = this.head;
+        while (tempNode != null)
+        {
+            if (tempNode == searchKey)
+            {
+                return tempNode;
+            }
+            tempNode = tempNode.getNext();
+        }
+        return null;
+    }
+
+    public INode insertBetweenNodes(INode head, INode newNode, int position)
+    {
+        INode previous = head;
+        int count = 1;
+        while (count < position - 1)
+        {
+            previous = previous.getNext();
+            count++;
+        }
+        INode current = previous.getNext();
+        newNode.setNext(current);
+        previous.setNext(newNode);
+        return head;
+    }
+
+    public INode deleteFromSequence(INode head, int position)
+    {
+        INode previous = head;
+        int count = 1;
+        while (count < position - 1)
+        {
+            previous = previous.getNext();
+            count++;
+        }
+        INode current = previous.getNext();
+        previous.setNext(current.getNext());
+        current = null;
+        lengthOfNode(head);
+        return current;
+    }
+    public int lengthOfNode(INode head)
+    {
+        INode tempNode = head;
+        int setNum = 0;
+        while (tempNode!=null)
+        {
+            tempNode=tempNode.getNext();
+            setNum++;
+        }
+        return  setNum;
+    }
+
+}
