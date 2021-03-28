e7ee0d3caef67f35873419e566baa88b80d12bfc
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-28 20:30:26 +0530

    [Prajakta]: Added pop and peak method in stack file

--- src/test/java/com/datastructure/stack/MyStackTest.java
+++ src/test/java/com/datastructure/stack/MyStackTest.java
@@ -22,7 +22,8 @@ public class MyStackTest
 
     }
     @Test
-    public void given3NumbersInStack_WhenPoped_ShouldMatchWithLastAddedNode() {
+    public void given3NumbersInStack_WhenPoped_ShouldMatchWithLastAddedNode()
+    {
         MyStack myStack = new MyStack();
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
