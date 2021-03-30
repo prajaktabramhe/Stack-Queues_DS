1c07818b472295df76f1aa9f702d9cbd84e51ddc
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-30 12:03:39 +0530

    [Prajakta]: Dequeue the node used Linkedlist to the Queue Operation

--- src/test/java/com/datastructure/stack/QueueTest.java
+++ src/test/java/com/datastructure/stack/QueueTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class QueueTest
 {
     @Test
-    public void given3Integers_WhenAddedToQueue_ShouldPassTheTest(){
+    public void given3Integers_WhenAddedToQueue_ShouldPassedTheTest(){
         MyQueue myQueue = new MyQueue();
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
@@ -17,5 +17,19 @@ public class QueueTest
         INode peak = myQueue.peak();
         myQueue.printQueue();
         Assertions.assertEquals(peak, myFirstNode);
+    }
+    @Test
+    public void given3IntegersInQueue_ShouldDequeueTheFirstNode(){
+        MyQueue myQueue = new MyQueue();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.printQueue();
+        INode dequeue = myQueue.dequeue();
+        myQueue.printQueue();
+        Assertions.assertEquals(dequeue, myFirstNode);
     }
 }
